4d67dfd5ccf53141d3f2f0b5375c3410c8f7f8ec
Dylanramper <44001627+Dylanramper@users.noreply.github.com>
2021-10-31 16:13:40 -0400

    Movement/Bounds

--- SkallyWag/Assets/P_Move.cs
+++ SkallyWag/Assets/P_Move.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class P_Move : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player;
+    private Vector2 screenBounds;
+    public Camera mainCam;
+
+    private float shipWidth;
+    private float shipHeight;
+
     void Start()
     {
-        
+        //Getting Height and Width of screenPos and W/H of Player.
+        screenBounds = mainCam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCam.transform.position.z));
+        shipWidth = transform.GetComponent<BoxCollider2D>().bounds.extents.x;
+        shipHeight = transform.GetComponent<BoxCollider2D>().bounds.extents.y;
     }
+    
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Setting Bounds for the screen, relative to the player.
+        Vector3 cameraPos = transform.position;
+        cameraPos.x = Mathf.Clamp(cameraPos.x, screenBounds.x * -1 + shipWidth, screenBounds.x - shipWidth);
+        cameraPos.y = Mathf.Clamp(cameraPos.y, screenBounds.y * -1 + shipHeight, screenBounds.y - shipHeight);
+
+        transform.position = cameraPos;
+
+        Controls();
+    }
+
+
+    //Controls for Movement
+    public void Controls()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.Translate(new Vector2(0, 10 * Time.deltaTime));   
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.Translate(new Vector2(0, -10 * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Translate(new Vector2(-10 * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Translate(new Vector2(10 * Time.deltaTime, 0));
+        }
     }
 }
