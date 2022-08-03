a73d1303d787f1f126fe511b4c06695f7f44267f
Dylanramper <44001627+Dylanramper@users.noreply.github.com>
2022-08-03 18:08:18 -0400

    start powerups

--- SkallyWag/Assets/Scripts/Enemies/E_Bhvr.cs
+++ SkallyWag/Assets/Scripts/Enemies/E_Bhvr.cs
@@ -6,6 +6,8 @@ public class E_Bhvr : MonoBehaviour
 {
     float moveSpeed = 5.0f;
     float killZ = -8.48f;
+    public GameObject life;
+    int spawnRate;
 
     // Update is called once per frame
     void Update()
@@ -16,6 +18,14 @@ public class E_Bhvr : MonoBehaviour
         if(gameObject.transform.position.y <= killZ) 
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            
         }
     }
 }
