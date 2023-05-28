3f6265a729609b312a5cd4d74e2ada883c9ada6b
Dylanramper <44001627+Dylanramper@users.noreply.github.com>
2023-05-28 18:28:24 -0400

    started control overhall

--- SkallyWag/Assets/Scripts/P_Drag.cs
+++ SkallyWag/Assets/Scripts/P_Drag.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class P_Drag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 touchPos;
+    public Rigidbody2D rb;
+    private Vector3 direction;
+    private float moveSpeed = 10f;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPos = Camera.main.ScreenToViewportPoint(touch.position);
+            touchPos.z = 0;
+            direction = (touchPos - transform.position);
+            rb.velocity = new Vector2(direction.x, direction.y) * moveSpeed;
+
+            if(touch.phase == TouchPhase.Ended)
+                rb.velocity = Vector2.zero;
+            
+        }
     }
 }
